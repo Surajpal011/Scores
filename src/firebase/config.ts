ee59694f7f308e973f463525da36db9c97c674e3
Surajpal011 <149008391+Surajpal011@users.noreply.github.com>
2025-06-21 00:44:16 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -11,13 +11,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBDQ5c9j2FX9Ul53ZmYs7A1AhSTif_zfDw",
-  authDomain: "realtimescores-8bef6.firebaseapp.com",
-  projectId: "realtimescores-8bef6",
-  storageBucket: "realtimescores-8bef6.firebasestorage.app",
-  messagingSenderId: "64398107481",
-  appId: "1:64398107481:web:3f7400093a4f4248312248",
-  measurementId: "G-9H4XZHKR66",
+  apiKey: "your_fire_base_api_key",
+  authDomain: "your_fire_base_auth_domain",
+  projectId: "your_fire_base_project_id",
+  storageBucket: "your_fire_base_storage_Bucke",
+  messagingSenderId: "your_fire_base_messaging_Sender_Id",
+  appId: "your_appId_app_Id",
+  measurementId: "your_fire_base_measurement_Id",
 };
 
 // Initialize Firebase
